b9d5c7b995fc83421051801af8e690d4d59ad148
nok1214 <80727744+nok1214@users.noreply.github.com>
2021-04-17 00:16:27 -0700

    rewrite removeDuplicates function with comparing method

--- Problem4.js
+++ Problem4.js
@@ -47,3 +47,20 @@ function removeDuplicates(num) {
 }
 
 console.log(removeDuplicates(myNumbers));                   //return [ '1', '2', '3', '4', '5', '6', '7', '8' ];
+
+
+//rewrite remove duplicate items function;
+
+function removeDuplicates(arr) {
+    var arr1 = [];                                          //create an empty array to compare with the original array;
+    for(let i = 0; i < arr.length; i++) {
+        if(!arr1.includes(arr[i])) {                        //check to make sure out new array(arr1) is not include inside of our original array(arr);
+            arr1.push(arr[i]);                              //if its not include inside our new array(arr1), use .push() to add the value into our new array(arr1);
+        };
+    };
+    return arr1.sort(function (a, b) {                      //useing .sort() with a compare function to sort our array in an ascending order;
+        return a - b;
+    });
+};
+
+console.log(removeDuplicates(myNumbers));                   //output [ 1, 2, 3, 4, 5, 6, 7, 8 ];

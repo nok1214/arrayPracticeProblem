50da9f44b76861d864aa7c59c942e7a9b66aabce
nok1214 <80727744+nok1214@users.noreply.github.com>
2021-04-16 23:53:39 -0700

    added a parameter to the input array that is sorting

--- Problem3.js
+++ Problem3.js
@@ -11,11 +11,11 @@ but, with a compare function, we can sort through vlaues Alphabetically, Numeric
 */
 var arr1 = [-3,8,7,6,5,-4,3,2,1,23,16,66,178,235,5677,-14123,0,-44,-21];
 
-function sortNumber(){
-    arr1.sort(function(a,b){            //using .sort() method with a compare function will get the correct result;
+function sortNumber(arr){
+    arr.sort(function(a,b){            //using .sort() method with a compare function will get the correct result;
         return a - b                    //using a-b to tell our function to sort our value numerically and not as a string;
     });
-    return arr1;
+    return arr;
 }
 
-console.log(sortNumber());              //output: [-14123, -44, -21, -4, -3, 0, 1, 2, 3, 5, 6, 7, 8, 16, 23, 66, 178, 235, 5677];
+console.log(sortNumber(arr1));              //output: [-14123, -44, -21, -4, -3, 0, 1, 2, 3, 5, 6, 7, 8, 16, 23, 66, 178, 235, 5677];

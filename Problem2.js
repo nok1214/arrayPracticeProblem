f8035675d14db7d1a685afeca9eb56e53e443dca
nok1214 <80727744+nok1214@users.noreply.github.com>
2021-04-16 23:50:33 -0700

    added function to join all element of the array into a string

--- Problem2.js
+++ Problem2.js
@@ -12,6 +12,15 @@ No.2: using .join() to join the items inside the array together;
 //Code starts here
 
 var myColor = ["Red", "Green", "White", "Black"];           //example array;
-console.log(myColor.toString());                            //use .toString() to join the items inside our array as one string;
-console.log(myColor.join(","));                             //use .join() to join the items inside our array as one string, need to provide separator;
-console.log(myColor.join("-"));                             //defalut means no separator which will connect everything inside the array together;
+console.log(myColor.toString());                            //Output Red,Green,White,Black; Use .toString() to join the items inside our array as one string;
+console.log(myColor.join(","));                             //Output Red,Green,White,Black; Use .join() to join the items inside our array as one string, need to provide separator;
+console.log(myColor.join("-"));                             //Output Red-Green-White-Black; Defalut means no separator which will connect everything inside the array together;
+
+
+//using .toString() to join element together;
+
+function joinElement(arr) {
+    return arr.toString();
+};
+
+console.log(joinElement(myColor));                          //output Red,Green,White,Black
